b80915d64aeb6d1f7914ca50329a9ec29c110bb9
RezerF <pavelp5668@gmail.com>
2021-10-29 10:21:03 +0300

    a

---
+++ calculate.py
@@ -0,0 +1,36 @@
+new_data = {
+    "cold_water": 2,
+    "hot_water": 1,
+    "electricity": 123,
+    "heat": 213,
+}
+old_data = {
+    "cold_water": "",
+    "hot_water": "",
+    "electricity": "",
+    "heat": "",
+}
+
+tariffs = {
+    "water_in": 2,
+    "water_out": 2,
+    "heating_water": 2,
+    "electricity": 2,
+    "heating": 2,
+}
+
+
+def calculate_consumption():
+    # return data
+    pass
+
+
+def send_data():
+    pass
+
+
+def calculate_money():
+    pass
+
+
+
